dd29264f33692e091cfa276da7a5cc8be60959d5
Jordan Morales <jordan.morales@gmail.com>
2020-02-02 04:11:58 -0800

    carousel component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Body from './components/body'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends React.Component {
--- src/components/body.js
+++ src/components/body.js
@@ -5,10 +5,10 @@
 import React from 'react'
 
 // components
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-
+import Carousel from 'react-bootstrap/Carousel'
 
 // =============================
 // COMPONENT CLASS
@@ -31,19 +31,45 @@ render(){
       <Grid item xs={12} className="header">
         Banner and Info
       </Grid>
-        <Grid item xs={6} className="carousel">
-        <Paper>
-        Image Carousel This is where some images goes
-        <br />
-        <img
-        src="https://res.cloudinary.com/htc0pkenr/image/upload/c_scale,w_471/v1578445797/kcw5rjnz29awou0chama.png"
-        alt="compendium"
-        >
-        </img>
-        </Paper>
+        <Grid item xs={8} className="carousel">
+
+
+        <Carousel>
+          <Carousel.Item>
+            <img
+            src="https://res.cloudinary.com/htc0pkenr/image/upload/c_scale,w_471/v1578445797/kcw5rjnz29awou0chama.png"
+            alt="compendium"
+            />
+            <Carousel.Caption>
+            Image Carousel This is where some images goes
+            <a href="https://complete-compendium.herokuapp.com/" alt="link to compendium project"><ion-icon name="link" size="large"></ion-icon>
+            </a>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+            src="https://res.cloudinary.com/htc0pkenr/image/upload/c_scale,w_471/v1578445797/kcw5rjnz29awou0chama.png"
+            alt="compendium"
+            />
+            <Carousel.Caption>
+            Image Carousel This is where some images goes
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+            src="https://res.cloudinary.com/htc0pkenr/image/upload/c_scale,w_471/v1578445797/kcw5rjnz29awou0chama.png"
+            alt="compendium"
+            />
+            <Carousel.Caption>
+            Image Carousel This is where some images goes
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+
+
         </Grid>
 
-        <Grid item xs={6} className="text">
+        <Grid item xs={4} className="text">
         Description This is where some text about stuff goes
         </Grid>
       <Grid item xs={12} className="footer">
